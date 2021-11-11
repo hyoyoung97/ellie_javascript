d938d28aa639a49c919024de4e94a91554df377d
hyoyoung97 <hyoyoung97@naver.com>
2021-11-11 18:14:49 +0900

    211111_second

--- variable.js
+++ variable.js
@@ -5,8 +5,39 @@ console.log('Hello world!');
 'use strict';
 
 // 2. Variable
-// let (added on ES6)
-let name = 'ayoung';
+// let (added on ES6), mutable
+let glonalName = 'global name'
+{
+  let name = 'ayoung';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+  console.log(glonalName);
+}
 console.log(name);
-name = 'hello';
-console.log(name);
+console.log(glonalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration frim bottom to top)
+// has no block scope
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
+// 3. Constants
+// favor immutable data type always for a few reasons;
+// -security
+// -thread safety
+// reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primtive, single item: number, string, boolean, null, undifined, symbol
+// object, box container
+// function, first-class function
+
+const count = 17  //integer
+const 
