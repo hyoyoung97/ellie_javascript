e41a6edcc2a909d16cc4685744fdb007ea593731
hyoyoung97 <hyoyoung97@naver.com>
2021-11-11 16:09:41 +0900

    first commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+console.log('Hello world!');
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript
+'use strict';
+
+// 2. Variable
+// let (added on ES6)
+let name = 'ayoung';
+console.log(name);
+name = 'hello';
+console.log(name);
